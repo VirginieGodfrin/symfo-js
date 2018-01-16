c037bf14c5d0d5944458b80807830bf8d349cc68
VirginieGodfrin <virginiegodfrin@gmail.com>
2018-01-16 14:53:40 +0100

    chap25 js templating

--- web/assets/js/repLogApp.js
+++ web/assets/js/repLogApp.js
@@ -99,7 +99,11 @@
             $form[0].reset();
         },
         _addRow: function(repLog) { 
-            console.log(repLog);
+            var tplText = $('#js-rep-log-row-template').html();
+            var tpl = _.template(tplText);
+            var html = tpl(repLog);
+            this.$wrapper.find('tbody').append($.parseHTML(html));
+            this.updateTotalWeightLifted();
         }
     });
     // obj Helper
